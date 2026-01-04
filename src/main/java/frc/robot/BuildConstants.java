65e35ad0e6e9c3ae6eed10bca86c2509a00a883f
Justin Nesselrotte <admin@jnesselr.org>
2026-01-03 20:01:56 -0700

    Getting project to compile

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "Quaternion";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 2;
+  public static final String GIT_SHA = "0f629c28eac7a3ab154ac5a62aa3f543692c6508";
+  public static final String GIT_DATE = "2026-01-03 21:49:11 EST";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2026-01-03 22:01:44 EST";
+  public static final long BUILD_UNIX_TIME = 1767495704457L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants(){}
+}
