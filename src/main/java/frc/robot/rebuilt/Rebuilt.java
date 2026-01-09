c2807a63e27a34ce747fa4da00984cc08c86169c
Justin Nesselrotte <admin@jnesselr.org>
2026-01-08 23:25:01 -0700

    Added Denbot FoxFlow

---
+++ src/main/java/frc/robot/rebuilt/Rebuilt.java
@@ -0,0 +1,10 @@
+package frc.robot.rebuilt;
+
+import bot.den.foxflow.RobotState;
+import bot.den.foxflow.StateMachine;
+
+@StateMachine
+public record Rebuilt(
+        RobotState robotState
+) {
+}
