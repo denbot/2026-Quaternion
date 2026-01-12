2559e2a3865255d98f6b786b2b50b59f01c343f0
Justin Nesselrotte <admin@jnesselr.org>
2026-01-11 23:31:40 -0700

    Fixing BuildConstants generation

--- src/main/java/bot/den/frc2026/quaternion/Robot.java
+++ src/main/java/bot/den/frc2026/quaternion/Robot.java
@@ -7,6 +7,7 @@
 
 package bot.den.frc2026.quaternion;
 
+import bot.den.frc2026.quaternion.generated.BuildConstants;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import org.littletonrobotics.junction.LogFileUtil;
